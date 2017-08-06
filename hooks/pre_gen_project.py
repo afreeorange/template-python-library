39d03017228b04fb9ea747cb87a0c372e57d78ff
Nikhil Anand <mail@nikhil.io>
2017-08-06 10:35:30 -0500

    Fix setuptools version bug

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,18 +1,26 @@
 from __future__ import print_function
 
 from string import ascii_letters
+import pip
 import sys
 
-
+# Check for a valid package name
 cleaned_package_name = ''.join(
     filter(
         lambda x: x in ascii_letters + '_', '{{cookiecutter.package_name}}'
     )
 )
 
-if cleaned_package_name != '{{cookiecutter.package_name}}':
-    print("""
+invalid_message = """
 Invalid package name!
 Can only contain alphabets (upper or lowercase) and underscores
-""")
+"""
+
+if cleaned_package_name != '{{cookiecutter.package_name}}':
+    print(invalid_message)
     sys.exit(1)
+
+# Upgrade setuptools to avoid version issues when running
+# coverage or linting tests
+pip.main(['install', '--upgrade', 'setuptools'])
+
